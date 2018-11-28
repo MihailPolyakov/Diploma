302b13de33bc5f7a0d6461288f6409c6f5dc018b
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-11-28 14:59:37 +0800

    Delete SelectCategories.php

--- Diploma/models/SelectCategories.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once '../models/connectDB.php';
-$categories = "SELECT id, category FROM categories";
-function insertCategory($value) {
-	$pdo = new PDO("mysql:host=localhost;dbname=diploma_php", "Miha", "Qwerty123");
-	$insertCategory = $pdo->prepare("INSERT INTO categories (category) VALUES (:category)");
-	$insertCategory -> bindParam(':category', $value);
-	$insertCategory -> execute();
-};
