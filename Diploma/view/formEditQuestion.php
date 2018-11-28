047a2f4fc658e9a922bb33cc776de21f5804784d
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-11-28 14:57:29 +0800

    Delete formEditQuestion.php

--- Diploma/view/formEditQuestion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();?>
-<form action="../controllers/controllerAdminQA.php" method="POST">
-	<input type="hidden" name="idQuestion" value="<?php echo $_SESSION['idQuestion']?>">
-	<input type="text" name="editQuestion" placeholder="Введите свой вопрос">
-	<input type="submit" value="Изменить">
-</form>
