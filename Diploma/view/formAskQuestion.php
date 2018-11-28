9c9ff0b448f46535e26d5bc51fa795dfea0a8c6e
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-11-28 14:58:04 +0800

    Delete formAskQuestion.php

--- Diploma/view/formAskQuestion.php
+++
@@ -1,6 +0,0 @@
-<form action="../controllers/controllerUserQA.php" method="POST">
-	<input type="text" name="question" placeholder="Напишите свой вопрос">
-	<input type="text" name="name" placeholder="Напишите своей имя">
-	<input type="text" name="mail" placeholder="Напишите свой mail">
-	<input type="submit" value="Отправить">
-</form>
