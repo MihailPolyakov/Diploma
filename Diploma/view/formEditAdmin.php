e626a865348e237eccb8254e5eeb2617413bd532
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-11-28 14:57:47 +0800

    Delete formEditAdmin.php

--- Diploma/view/formEditAdmin.php
+++
@@ -1,6 +0,0 @@
-<form action="" method="GET">
-	<input type="hidden" name="id" value="<?php echo $_GET['editAdmin']?>">
-	<input type="text" name="editlogin">
-	<input type="text" name="editpassword">
-	<input type="submit" value="Изменить">
-</form>
