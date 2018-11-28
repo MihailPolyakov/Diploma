5247ddaef748c63db43c3fda2594ef8307e7e33e
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-11-28 14:57:07 +0800

    Delete loginadmin.php

--- Diploma/view/loginadmin.php
+++
@@ -1,5 +0,0 @@
-<form action="../controllers/controllerAdminHP.php" method="POST">
-	<input type="text" name="login" placeholder="введите логин">
-	<input type="text" name="password" placeholder="введите пароль">
-	<input type="submit" name="войти">
-</form>
