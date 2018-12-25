4694fb171ae61a8cdb712d3aa648ea5a14aa7a1a
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-12-25 18:11:01 +0800

    Add files via upload

--- Diploma/index.php
+++ Diploma/index.php
@@ -4,53 +4,117 @@ session_start();
 require "models/connectDB.php";
 require "models/autoload.php";
 
-//проверка на присутствие гет параметра с именем контроллера, отправляются при переходе на другой контроллер или выходе на главную страницу
-if (!empty($_GET['controller'])) {
-	$_SESSION['controller'] = $_GET['controller'];
-}
-//проверка на присутствие пост запроса с именем логина, для обнуления сессии
-if (!empty($_POST['login'])) {
-	$_SESSION['login'] = NULL;
-}
-//проверка на присутствие параметров, по которым можно определить, что нужно вызвать контроллер с вопросами и ответами для Администратора
-if ((!empty($_GET['idcatadmin']) && !empty($_GET['category'])) || $_SESSION['controller'] == 'AdminQA') {	
-	
-	$homePageAdmin = new ControllerAdminHP($pdo);
-	$homePageAdmin -> transferAnotherController();
-	
-	$questionAnswers = new ControllerAdminQA($pdo);
-	$questionAnswers -> deleteQuestion();
-	$questionAnswers -> editQuestion();
-	$questionAnswers -> newQuestion();
-	$questionAnswers -> category();
-	$questionAnswers -> hiddenQuestion();
-	$questionAnswers -> publishedQuestion();
-	$questionAnswers -> editUser();
-	$questionAnswers -> editAnswer();
-	$questionAnswers -> postNewAnswer();
-	$questionAnswers -> showQuestionsAnswers();
-//проверка на присутствие параметров, по которым можно определить, что нужно вызвать контроллер с домашней для Администратора
-} elseif (!empty($_POST['login']) ||  $_SESSION['controller'] == 'AdminHP') {
-	$homePageAdmin = new ControllerAdminHP($pdo);
-	$homePageAdmin -> newCategory();
-	$homePageAdmin -> deleteAdmin();
-	$homePageAdmin -> newLogin();
-	$homePageAdmin -> editLogin();
-	$homePageAdmin -> editAdmin();
-	$homePageAdmin -> deleteCategory();
-	$homePageAdmin -> categories();
-//проверка на присутствие параметров, по которым можно определить, что нужно вызвать контроллер с вопросами и ответами для юзера	
-} elseif ((!empty($_GET['idcat']) && !empty($_GET['category'])) || $_SESSION['controller'] == 'UserQA') {
-	$user = new ControllerUserHP($pdo);
-	$user -> controllerUserQA();
+$user = new ControllerUserHP($pdo);
+$homePageAdmin = new ControllerAdminHP($pdo);
+$questionAnswers = new ControllerAdminQA($pdo);
+$idCategory = new ControllerUserQA($pdo);
 
-	$idCategory = new ControllerUserQA($pdo);
-	$idCategory -> insertQuestion();
-	$idCategory -> askQuestion();
-	$idCategory -> questionsAnswers();	
-	//в случае если не одно условие не выполнелось вызывается контроллер с домашней страницей
-} else {
-	$user = new ControllerUserHP($pdo);
-	$user -> homePage();
-}
+	if (!empty($_GET['idcatadmin']) && !empty($_GET['category'])) {
+	//вызвать функцию для отображения вопросов и ответтов для администартора		
+		$homePageAdmin -> transferAnotherController();
+		$questionAnswers -> showQuestionsAnswers();
+	}
 
+	if (!empty($_POST['login'])) {
+	//вызвать функцию для отображения домашней страницы для администратора	
+		$homePageAdmin -> categories();	
+	}
+
+	if (!empty($_GET['admin'])){
+		$user -> loginadmin();
+	}
+
+	if ((!empty($_GET['idcat']) && !empty($_GET['category']))) {
+	//вызвать функцию для отображения вопросов и ответтов для пользователя	
+		$user -> controllerUserQA();
+		$idCategory -> questionsAnswers();		
+	}
+
+	if (empty($_GET) && empty($_POST)) { 
+		//вызвать функцию для отображения домашней страницы
+		$user -> homePage();
+	}
+
+
+	if (!empty($_POST['deleteAdmin'])) {
+		$homePageAdmin -> deleteAdmin();
+		$homePageAdmin -> categories();
+	};
+
+	if (!empty($_POST['newcategory'])) {
+		$homePageAdmin -> newCategory();
+		$homePageAdmin -> categories();
+	};	
+	
+	if (!empty($_POST['newlogin'])) {
+		$homePageAdmin -> newLogin();
+		$homePageAdmin -> categories();
+	};
+	
+	if (!empty($_GET['editAdmin'])) {
+		$homePageAdmin -> editAdmin();
+		$homePageAdmin -> categories();
+	};
+
+	if (!empty($_GET['editlogin'])) {
+		$homePageAdmin -> editLogin();
+		$homePageAdmin -> categories();
+	};
+
+	if (!empty($_POST['deleteCategory'])) {
+		$homePageAdmin -> deleteCategory();
+		$homePageAdmin -> categories();
+	}
+	if (!empty($_GET['homepage'])) {
+		$homePageAdmin -> categories();
+	}
+
+
+
+	if (!empty($_GET['ask'])) {
+		$idCategory -> askQuestion();
+		$idCategory -> questionsAnswers();
+	}
+	
+	if (!empty($_POST['name'])) {
+		$idCategory -> insertQuestion();
+		$idCategory -> questionsAnswers();
+	}
+
+
+	if (!empty($_POST['deleteQuestion'])) {
+		$questionAnswers -> deleteQuestion();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_GET['editQuestion'])) {
+		$questionAnswers -> editQuestion();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['editQuestion'])) {
+		$questionAnswers -> newQuestion();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['category'])) {
+		$questionAnswers -> category();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['hiddenQuestion'])) {
+		$questionAnswers -> hiddenQuestion();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['publishedQuestion'])) {
+		$questionAnswers -> publishedQuestion();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['editUser'])) {
+		$questionAnswers -> editUser();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_GET['editAnswer'])) {
+		$questionAnswers -> editAnswer();
+		$questionAnswers -> showQuestionsAnswers();
+	}
+	if (!empty($_POST['editAnswer'])) {
+		$questionAnswers -> postNewAnswer();
+		$questionAnswers -> showQuestionsAnswers();
+	}
